6ce7354c9ba7808bb452853e5d75027a141b6774
A_tian <76234281+FloatXD@users.noreply.github.com>
2022-05-13 14:21:15 +0800

    update e2e (#43)
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    * update e2e
    
    Co-authored-by: 123 <123>

--- test/e2e/SmokeTest/ldm.go
+++ test/e2e/SmokeTest/ldm.go
@@ -14,14 +14,13 @@ import (
 
 var _ = ginkgo.AfterSuite(func() {
 	output := runInLinux("sh deletedisk.sh")
-	logrus.Info("delete disk ", output)
+	logrus.Info("delete disk", output)
 })
 
-var _ = ginkgo.Describe("test Local Disk Manager  ", ginkgo.Label("smokeTest"), func() {
+var _ = ginkgo.Describe("test Local Disk Manager", ginkgo.Label("pr"), func() {
 	f := framework.NewDefaultFramework(ldapis.AddToScheme)
 	client := f.GetClient()
 	ctx := context.TODO()
-
 	ginkgo.Context("test Local Disk", func() {
 		ginkgo.It("Configure the base environment", func() {
 			configureEnvironment(ctx)
@@ -33,19 +32,19 @@ var _ = ginkgo.Describe("test Local Disk Manager  ", ginkgo.Label("smokeTest"), 
 				f.ExpectNoError(err)
 			}
 			logrus.Printf("There are %d local volumes ", len(localDiskList.Items))
-			gomega.Expect(len(localDiskList.Items)).To(gomega.Equal(6))
+			gomega.Expect(len(localDiskList.Items)).To(gomega.Equal(9))
 		})
 		ginkgo.It("Manage new disks", func() {
 			output := runInLinux("sh adddisk.sh")
-			logrus.Printf("add  disk : %+v", output)
+			logrus.Info("add  disk :", output)
 			err := wait.PollImmediate(3*time.Second, 3*time.Minute, func() (done bool, err error) {
 				localDiskList := &ldv1.LocalDiskList{}
 				err = client.List(ctx, localDiskList)
 				if err != nil {
-					f.ExpectNoError(err)
+					logrus.Error("add disk failed")
 					logrus.Error(err)
 				}
-				if len(localDiskList.Items) != 7 {
+				if len(localDiskList.Items) != 10 {
 					return false, nil
 				} else {
 					logrus.Infof("There are %d local volumes ", len(localDiskList.Items))
--- test/e2e/deletedisk.sh
+++ test/e2e/deletedisk.sh
@@ -20,5 +20,5 @@ for h in $hosts; do
   ##查看硬盘序号
   # govc device.ls -vm $h
   ##删除硬盘
-  govc device.remove -vm $h -keep=false disk-1000-2
+  govc device.remove -vm $h -keep=false disk-1000-3
 done
