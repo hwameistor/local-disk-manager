971e126d9a36426b22081b037e40bbb30aa28957
123 <123>
2022-06-10 14:30:43 +0800

    Add logic to use the latest crd in E2E testing

--- test/e2e-test.sh
+++ test/e2e-test.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 git clone https://github.com/hwameistor/helm-charts.git test/helm-charts
+git clone https://github.com/hwameistor/local-storage.git test/local-disk-manager
+cp -r -f test/local-disk-manager/deploy/crds/* test/helm-charts/charts/hwameistor/crds/
 cat test/helm-charts/charts/hwameistor/values.yaml | while read line
 ##
 do
@@ -17,15 +19,15 @@ do
             image=$img:${line:5:50}
             echo "docker pull ghcr.io/$image"
             docker pull ghcr.io/$image
-            echo "docker tag ghcr.io/$image 10.6.170.180/$image"
+            echo "docker tag ghcr.io/$image $ImageRegistry/$image"
             docker tag ghcr.io/$image 10.6.170.180/$image
-            echo "docker push 10.6.170.180/$image"
+            echo "docker push $ImageRegistry/$image"
             docker push 10.6.170.180/$image
         fi
     fi
 done
 ##
-sed -i '/.*ghcr.io*/c\hwameistorImageRegistry: 10.6.170.180' test/helm-charts/charts/hwameistor/values.yaml
+sed -i '/.*ghcr.io*/c\hwameistorImageRegistry: '$ImageRegistry'' test/helm-charts/charts/hwameistor/values.yaml
 sed -i '/hwameistor\/local-disk-manager/{n;d}' test/helm-charts/charts/hwameistor/values.yaml
 sed -i '/hwameistor\/local-disk-manager/a \ \ \ \ tag: 99.9-dev' test/helm-charts/charts/hwameistor/values.yaml
 ginkgo --fail-fast --label-filter=${E2E_TESTING_LEVEL} test/e2e
